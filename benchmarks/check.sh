1fd9321f4a636056cf4eb802d72b9c98f7d0f3e4
wsheny <wshenyi@umich.edu>
2022-10-08 01:05:43 -0400

    Update diagram

--- benchmarks/check.sh
+++ benchmarks/check.sh
@@ -11,7 +11,9 @@ cd ~/eecs583/hw2/benchmarks/correctness || exit
 for file in ./*.c; do
   module=${file##*/}
   module=${module%.c}
-  ./run.sh "$module" &> $(pwd)/"$module".log
+  ../run.sh "$module" &> $(pwd)/"$module".log
+  ../viz.sh "$module" &> /dev/null
+  ../viz.sh "$module".fplicm &> /dev/null
   num=$(($num+1))
   if grep -c "PASS" $(pwd)/"$module".log > /dev/null; then
     cor=$(($cor+1))
@@ -20,12 +22,15 @@ for file in ./*.c; do
     echo -e "$module" "${RED}PASSED${NC}"
   fi
 done
+rm -f default.profraw *_prof *_fplicm *_no_fplicm *.bc *.profdata *_output *.ll
 
 cd ~/eecs583/hw2/benchmarks/performance || exit
 for file in ./*.c; do
   module=${file##*/}
   module=${module%.c}
-  ./run.sh "$module" &> $(pwd)/"$module".log
+  ../run.sh "$module" &> $(pwd)/"$module".log
+  ../viz.sh "$module" &> /dev/null
+  ../viz.sh "$module".fplicm &> /dev/null
   num=$(($num+1))
   if grep -c "PASS" $(pwd)/"$module".log > /dev/null; then
     cor=$(($cor+1))
@@ -34,4 +39,5 @@ for file in ./*.c; do
     echo -e "$module" "${RED}FAILED${NC}"
   fi
 done
+rm -f default.profraw *_prof *_fplicm *_no_fplicm *.bc *.profdata *_output *.ll
 echo Results: $cor/$num
